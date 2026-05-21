743f8b1a3451a384f1287d4ee8c8389d3fa2b178
w1nReach <nikits0409gg@gmail.com>
2026-05-21 20:59:37 +0300

    Add files via upload

---
+++ вадим/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{padding:"40px",textAlign:"center"}}>
+      <h1>Свяжи физику</h1>
+      <p>Проект готов для Vercel и GitHub.</p>
+    </div>
+  );
+}
