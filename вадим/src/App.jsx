3cce7273bbf7e75e98a5a82cf10eb791775825c4
w1nReach <nikits0409gg@gmail.com>
2026-05-21 21:31:38 +0300

    Delete вадим directory

--- вадим/src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{padding:"40px",textAlign:"center"}}>
-      <h1>Свяжи физику</h1>
-      <p>Проект готов для Vercel и GitHub.</p>
-    </div>
-  );
-}
